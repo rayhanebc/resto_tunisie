3da1fa6bd9bc8c5666d42a52e8e01c9458cf3261
Rayhane <Rayhane@Rayhane-PC>
2014-02-17 21:23:31 +0100

    new commit by Rayhane : creation des packages

--- src/restotunisie/DAO/ReservationDAO.java
+++ src/restotunisie/DAO/ReservationDAO.java
@@ -2,12 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package restotunisie;
+package restotunisie.DAO;
 
 /**
  *
  * @author Rayhane
  */
-public class FactureDAO implements InterfaceFactureDAO{
+public class ReservationDAO implements InterfaceReservationDAO{
     
 }
--- src/restotunisie/entities/Reservation.java
+++ src/restotunisie/entities/Reservation.java
@@ -2,12 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package restotunisie;
+package restotunisie.entities;
 
 /**
  *
  * @author Rayhane
  */
-public interface InterfaceFactureDAO {
+public class Reservation {
     
 }
