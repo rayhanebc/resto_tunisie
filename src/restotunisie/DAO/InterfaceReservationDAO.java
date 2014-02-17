bf454ae4dd890409850ad6588a68e85fefdfb33b
Rayhane <Rayhane@Rayhane-PC>
2014-02-17 22:25:36 +0100

    new commit by Rayhane : creation des entites facture et reservation + interfaces + DAOs

--- src/restotunisie/DAO/InterfaceReservationDAO.java
+++ src/restotunisie/DAO/InterfaceReservationDAO.java
@@ -4,10 +4,18 @@
  */
 package restotunisie.DAO;
 
+import java.util.List;
+import restotunisie.entities.Reservation;
+
 /**
  *
  * @author Rayhane
  */
 public interface InterfaceReservationDAO {
+     public void createReservation(Reservation r);
+    public void updateReservation(Reservation r);
+    public void deleteReservation(Reservation r);
+    public Reservation findReservation(int id);
+    public List<Reservation> displayAllReservation();
     
 }
