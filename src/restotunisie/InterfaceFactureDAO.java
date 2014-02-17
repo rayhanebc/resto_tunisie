9af1c519cb17c6ad06c87486d8ecce2217fe36ad
Rayhane <Rayhane@Rayhane-PC>
2014-02-17 10:53:47 +0100

    new commit by Rayhane

---
+++ src/restotunisie/InterfaceFactureDAO.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restotunisie;
+
+/**
+ *
+ * @author Rayhane
+ */
+public interface InterfaceFactureDAO {
+    
+}
